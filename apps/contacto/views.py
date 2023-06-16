5781e3f3721c14bb4e9f4ac3d10690ac8aa066a5
leanlino <lteclino3@gmail.com>
2023-06-16 19:22:31 -0300

    version 16

--- apps/contacto/views.py
+++ apps/contacto/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def contact(request):
+    return render(request, 'pages/contact.html')
+
+def submit(request):
+    if request.method == 'POST':
+        print(request.POST)
+        
+        return redirect('home')
     return render(request, 'pages/contact.html')
